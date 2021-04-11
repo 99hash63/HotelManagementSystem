3d3f91536d2c292b44e8a74839dc2e23e7dd7dae
Oshada1988 <lahirupr471@gmail.com>
2021-04-11 19:14:42 +0530

    Create Final Bill Model and Routes

---
+++ backend/models/Bill.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const Bills = new Schema({
+    CusName: {
+        type: String,
+        require: true,
+    },
+    NIC: {
+        type: String,
+        require: true,
+        
+    },
+    Allocationa_Amount: {
+        type: Number,
+        require: true,
+       
+    },
+    Mail: {
+        type: String,
+        require: true,
+        
+       
+    },
+    Meal_Order_Cost: {
+        type: String,
+        require: true,
+       
+    },
+   BarOrder_Cost: {
+        type: Number,
+        require: true,
+       
+    },
+    Additional_Bill: {
+        type: Number,
+        require: true,
+       
+    },
+    Final_Cost: {
+        type: Number,
+        require: true,
+       
+    },
+})
+const fbil = mongoose.model("Bill",Bills);
+
+module.exports =fbil;
