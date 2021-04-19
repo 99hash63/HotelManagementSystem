4556498e99d951ce2b68087be88f983f5477c2c6
unknown <ishani9799@gmail.com>
2021-04-19 22:00:15 +0530

    Updated room models/routes and server.js

--- backend/routes/roomType.js
+++ backend/routes/roomType.js
@@ -25,6 +25,67 @@ router.route('/get').get((req, res) => {
 // });
 
 
+//Ishara........
+
+
+//update roomType
+
+router.route("/update/:id").put(async (req,res) =>{
+    let roomTypeId = req.params.id;
+    const {typeName, capacity, AC, FullBoardPrice, HalfBoardPrice, BedAndBreakfastPrice} = req.body;
+
+    const updateroomType = {
+        typeName,
+        capacity,
+        AC,
+        FullBoardPrice,
+        HalfBoardPrice,
+        BedAndBreakfastPrice
+    }
+
+   roomType.findByIdAndUpdate (roomTypeId, updateroomType).then((roomType) =>{
+        res.json(roomType)
+
+    }).catch((err) => {
+        console.log(err);
+        res.json(err)
+    
+    
+
+})
+}) 
+
+//delete roomType
+
+router.route("/delete/:id").delete(async (req,res) => {
+    let roomTypeId = req.params.id;
+
+    await roomType.findByIdAndDelete(roomTypeId).then(() => {
+        res.status(200).send({status: "roomType deleted"});
+
+    }).catch((error) => {
+        console.log(err.message);
+        res.status(500).send({status: "Error with delete user",error:err.message});
+
+
+    })
+
+})
+
+// get a roomType
+
+router.route("/get/:id").get(async (req,res) => {
+    let roomTypeId = req.params.id;
+  roomType.findById(roomTypeId).then((roomType) => {
+        res.json(roomType)
+
+    }).catch((err)=> {
+        console.log(err.message);
+        res.json(err)
+    })
+})
+
+
 
 
 module.exports = router;
