98024282eb6af20fa79b515f42c83265fea0b202
99hash63 <thkottegoda@gmail.com>
2021-04-20 01:54:18 +0530

    modified server cors function

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+}));
 app.use(bodyParser.json());
 
 app.get("/test", (req, res) => {
