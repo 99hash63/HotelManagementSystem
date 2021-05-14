fab6a69812b5c8deef3735d30bf441fe78f6a531
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-05-14 16:59:39 +0530

    Changes in ManagerLogin

--- frontend/src/components/ManagerLogin.js
+++ frontend/src/components/ManagerLogin.js
@@ -16,36 +16,36 @@ const ManagerLogin = () => {
     
         <div className="ManagerLoginBox">
             <div className="ManagerLoginBox-a ManagerLoginBoxBTN">
-            <i className="fas fa-warehouse"></i><br />
-            <span  id="manager-text">Inventory Manager</span>
+            <i className="fas"><img src="https://img.icons8.com/color/144/000000/front-desk.png"/></i><br />
+            <span  id="manager-text">Front Office Manager</span>
             </div>
             <div className="ManagerLoginBox-b ManagerLoginBoxBTN">
-            <i className="fas fa-warehouse"></i><br />
+            <i className="fas"><img src="https://img.icons8.com/color/144/000000/warehouse.png"/></i><br />
             <span  id="manager-text">Inventory Manager</span>
             </div>
             <div className="ManagerLoginBox-c ManagerLoginBoxBTN">
-            <i className="fas fa-warehouse"></i><br />
-            <span  id="manager-text">Inventory Manager</span>
+            <i className="fas"><img src="https://img.icons8.com/color/144/000000/room.png"/></i><br />
+            <span  id="manager-text">Room Manager</span>
             </div>
             <div className="ManagerLoginBox-d ManagerLoginBoxBTN">
-            <i className="fas fa-warehouse"></i><br />
-            <span  id="manager-text">Inventory Manager</span>
+            <i className="fas"><img src="https://img.icons8.com/color/144/000000/food.png"/></i><br />
+            <span  id="manager-text">Food Manager</span>
             </div>
             <div className="ManagerLoginBox-e ManagerLoginBoxBTN">
-            <i className="fas fa-warehouse"></i><br />
-            <span  id="manager-text">Inventory Manager</span>
+            <i className="fas"><img src="https://img.icons8.com/color/144/000000/commercial-development-management.png"/></i><br />
+            <span  id="manager-text">Employee Manager</span>
             </div>
             <div className="ManagerLoginBox-f ManagerLoginBoxBTN">
-            <i className="fas fa-warehouse"></i><br />
-            <span  id="manager-text">Inventory Manager</span>
+            <i className="fas"><img src="https://img.icons8.com/color/144/000000/home.png"/></i><br />
+            <span  id="manager-text">Home Page</span>
             </div>
             <div className="ManagerLoginBox-g ManagerLoginBoxBTN">
-            <i className="fas fa-warehouse"></i><br />
-            <span  id="manager-text">Inventory Manager</span>
+            <i className="fas"><img src="https://img.icons8.com/color/144/000000/bar.png"/></i><br />
+            <span  id="manager-text">Bar Manager</span>
             </div>
             <div className="ManagerLoginBox-h ManagerLoginBoxBTN">
-            <i className="fas fa-warehouse"></i><br />
-            <span  id="manager-text">Inventory Manager</span>
+            <i className="fas"><img src="https://img.icons8.com/color/144/000000/corridor.png"/></i><br />
+            <span  id="manager-text">Reception Hall Manager</span>
             </div>
         </div>
 
